8fd152c0462064bf2130564746f62738a1474500
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-20 21:22:46 +0100

    Update replace.py

--- replace.py
+++ replace.py
@@ -3,6 +3,6 @@ import os
 files_path = os.environ["INPUT_PATH"]
 files_filter = os.environ["INPUT_FILTER"]
 
-os.environ["GITHUB_OUTPUT"] = "test=output"
+os.environ["GITHUB_OUTPUT"] = "time = time"
 
 print(files_path + files_filter)
