6e6b4fe8da01cffe063ceaeb987de827a4d0672a
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-20 22:47:29 +0100

    Update replace.py

--- replace.py
+++ replace.py
@@ -3,8 +3,9 @@ import os
 files_path = os.environ["INPUT_PATH"]
 files_filter = os.environ["INPUT_FILTER"]
 output_file = os.environ["GITHUB_OUTPUT"]
-
-print(files_path)
     
 with open(output_file, "a") as file:
     file.write(f"time={files_filter}")
+
+lst = os.listdir("./")
+print lst
