75a38e9404ed2612861e2bb37a30f3c09e0130eb
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-21 10:54:08 +0100

    Update replace.py

--- replace.py
+++ replace.py
@@ -5,9 +5,9 @@ files_path = os.environ["INPUT_PATH"]
 files_filter = os.environ["INPUT_FILTER"]
 output_file = os.environ["GITHUB_OUTPUT"]
 
-with open(output_file, "a") as file:
-    file.write(f"time={files_filter}")
-
 z = files_path + "/" + files_filter
 y = glob.glob(z)
 print(y)
+
+with open(output_file, "a") as file:
+    file.write(f"time={len(y)}")
