abd00235051ab157b1127616bd87df399619656d
MrPotatoHead <66561475+PowerOps-MK@users.noreply.github.com>
2023-01-21 18:16:53 +0100

    Update replace.py

--- replace.py
+++ replace.py
@@ -7,11 +7,22 @@ output_file = os.environ["GITHUB_OUTPUT"]
 
 z = f"{files_path}/{files_filter}"
 y = glob.glob(z)
-for file in y:
-    with open(file, "r") as fil:
-        print(fil.read())
 
 modified_count = len(y)
-with open(output_file, "a") as fil:
+with open(output_file, "a") as file:
     o = f"count={modified_count}"
-    fil.write(o)
+    file.write(o)
+
+for k in y:
+    # Read in the file
+    with open(k, "r") as file:
+      filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("Hello", "xyz")
+
+    # Write the file out again
+    with open(k, "w") as file:
+        g = file.write(filedata)
+        print(g)
+
